dd0688f7e6b2c83bff6a17db9c1d42e3297ba17e
1nsurka <insur.kunafin@yandex.ru>
2023-08-17 21:36:18 +0300

    add changes history table

---
+++ backend.Migrator/Migrations/202307222348_CreateChangesHistory.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace backend.Migrator.Migrations;
+
+[Migration(202307222348)]
+public class CreateChangesHistory : Migration
+{
+    public override void Up()
+    {
+        const string sql = $@"
+            CREATE TABLE IF NOT EXISTS changes_history(
+                id BIGSERIAL,
+                target_id BIGINT,
+                history_type INTEGER,
+                text jsonb,
+                created_by BIGINT,
+                created_at TIMESTAMP WITH TIME ZONE,
+                approved_by BIGINT,
+                approved_at TIMESTAMP WITH TIME ZONE,
+                PRIMARY KEY(id, target_id, history_type)
+            );";
+
+        Execute.Sql(sql);
+    }
+
+    public override void Down()
+    {
+        Execute.Sql($"DROP TABLE IF EXISTS changes_history;");
+    }
+}
